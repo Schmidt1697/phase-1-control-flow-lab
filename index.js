b25c733acec72dfe90cd51f378f93447adc9311b
Anna Schmidt <marshall1697@gmail.com>
2022-08-29 16:13:34 -0600

    commit tree 64b44ffa44668670adecb759236e3d8690b3d976

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+  if (length < 400){
+    return "This one is on me!"
+  }else if (length >= 400 && length <= 2000){
+    return "That will be twenty bucks."
+  }else if (length > 2000 && length <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (length > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+     return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
+  
 }
